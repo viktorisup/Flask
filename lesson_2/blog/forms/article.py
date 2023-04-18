32cd4424bdcd68f4377e6eb5450e2d12de56ab3d
Viktor-mate <Viktor.isup@mail.ru>
2023-04-18 19:09:51 +0300

    lesson_8 completed

--- lesson_2/blog/forms/article.py
+++
@@ -1,8 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, TextAreaField, SubmitField, SelectMultipleField, validators
-
-class CreateArticleForm(FlaskForm):
-    title = StringField("Title", [validators.DataRequired()], )
-    body = TextAreaField("Body", [validators.DataRequired()], )
-    submit = SubmitField("Publish")
-    tags = SelectMultipleField("Tags", coerce=int)
--- lesson_2/blog/security.py
+++
@@ -1,6 +0,0 @@
-from flask_bcrypt import Bcrypt
-
-flask_bcrypt = Bcrypt()
-__all__ = [
-    "flask_bcrypt",
-]
