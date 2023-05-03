6306155570b3df9920c47ef499465f37b5be0ca5
Viktor-mate <Viktor.isup@mail.ru>
2023-05-03 15:34:44 +0300

    lesson_11 completed

--- lesson_2/blog/configs.py
+++ lesson_2/blog/configs.py
@@ -23,3 +23,7 @@ class DevConfig(BaseConfig):
 
 class TestingConfig(BaseConfig):
     TESTING = True
+
+class ProductionConfig(BaseConfig):
+    SQLALCHEMY_DATABASE_URI = os.environ.get("SQLALCHEMY_DATABASE_URI")
+    SECRET_KEY = os.environ.get("SECRET_KEY")
